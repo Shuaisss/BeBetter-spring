c05f0aee367cccea554e3207dc36dab87abe46fe
Changayeol <63961863+Changayeol@users.noreply.github.com>
2020-06-04 11:34:56 +0800

    Course提供模拟数据,待二次修改.

---
+++ src/main/java/cn/edu/scujcc/Course/CouresController.java
@@ -0,0 +1,23 @@
+package cn.edu.scujcc.Course;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/course")
+public class CouresController {
+	
+	@GetMapping
+	public List<Course> getAllCourse() {
+		return null;
+	}
+	
+	@GetMapping("/{id}")
+	public Course getCourse(String id) {
+		return null;
+	}
+
+}
---
+++ src/main/java/cn/edu/scujcc/Course/CourseService.java
@@ -0,0 +1,39 @@
+package cn.edu.scujcc.Course;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 提供模拟数据
+ * @author 86133
+ *
+ */
+public class CourseService {
+	private List<Course> course = new ArrayList<>();
+	
+	public CourseService() {
+		for(int i = 1;i<=10;i++) {
+			Course c = new Course();
+			c.setId("第"+i);
+			c.setName("第"+i+"节课程");
+			c.setPrice("价格为"+i*2);
+			c.setUrl("http://baidu.com");
+			course.add(c);
+		}
+	}
+	
+	public List<Course> getAllCourse(){
+		return this.course;
+	}
+	
+	public Course getCourse(String id) {
+		Course result = null;
+		for(Course c : this.course) {
+			if(id == c.getId()) {
+				result = c;
+				break;
+			}
+		}
+		return result;
+	}
+}

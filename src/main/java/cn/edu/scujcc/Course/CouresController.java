3d8a0716669d0e14171440d6dfa547f2148a4dd7
Changayeol <63961863+Changayeol@users.noreply.github.com>
2020-06-05 09:37:32 +0800

    新增Course的业务逻辑层

--- src/main/java/cn/edu/scujcc/Course/CouresController.java
+++ src/main/java/cn/edu/scujcc/Course/CouresController.java
@@ -2,7 +2,12 @@ package cn.edu.scujcc.Course;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,14 +15,39 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/course")
 public class CouresController {
+	@Autowired
+	private CourseService service;
 	
 	@GetMapping
 	public List<Course> getAllCourse() {
-		return null;
+		return service.getAllCourse();
 	}
 	
 	@GetMapping("/{id}")
-	public Course getCourse(String id) {
+	public Course getCourse(@PathVariable String id) {
+		return service.getCourse(id);
+	}
+	
+	/**
+	 * 删除一个指定的课程
+	 * @param id
+	 * @return
+	 */
+	@GetMapping("/{id}")
+	public ResponseEntity<String> deleteCourse(@PathVariable String id){
+		return ResponseEntity.ok().body("删除课程成功");
+	}
+	/**
+	 * 新建一个频道
+	 * @param c 新建频道的数据
+	 * @return 保存后的数据
+	 */
+	@PostMapping
+	public Course creatCourse(@RequestBody Course c) {
+		return null;
+	}
+	
+	public Course updateCourse(@RequestBody Course c) {
 		return null;
 	}
 
--- src/main/java/cn/edu/scujcc/Course/CourseService.java
+++ src/main/java/cn/edu/scujcc/Course/CourseService.java
@@ -3,29 +3,32 @@ package cn.edu.scujcc.Course;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.stereotype.Service;
+
 /**
  * 提供课程模拟数据
  * @author 86133
  *
  */
+@Service
 public class CourseService {
-	private List<Course> course = new ArrayList<>();
+	private List<Course> course;
 	
 	public CourseService() {
-		for(int i = 1;i<=10;i++) {
+		for(int i = 0;i<10;i++) {
 			Course c = new Course();
-			c.setId("第"+i);
-			c.setName("第"+i+"节课程");
-			c.setPrice("价格为"+i*2);
+			c.setId(""+(i + 1));
+			c.setName("第"+(i+1)+"节课程");
+			c.setPrice(""+(i+1)*(i+1));
 			c.setUrl("http://baidu.com");
-			course.add(c);
 		}
 	}
 	
-	public List<Course> getAllCourse(){
-		return this.course;
-	}
-	
+	/**
+	 * 新建一个频道
+	 * @param id
+	 * @return
+	 */
 	public Course getCourse(String id) {
 		Course result = null;
 		for(Course c : this.course) {
@@ -34,6 +37,28 @@ public class CourseService {
 				break;
 			}
 		}
+		
 		return result;
+	}
+	
+	/**
+	 * 更新一个频道
+	 * @param c
+	 * @return
+	 */
+	public Course updateCourse (Course c) {
+		Course toChange = getCourse(c.getId());
+		if(toChange != null) {
+			toChange.setName(c.getName());
+			toChange.setPrice(c.getPrice());
+			toChange.setUrl(c.getUrl());
+		}
+		return toChange;
+	}
+	
+	public Course creatCourse(Course c) {
+		c.setId(this.course.get(this.course.size()-1).getId()+1);
+		this.course.add(c);
+		return c;
 	}
 }

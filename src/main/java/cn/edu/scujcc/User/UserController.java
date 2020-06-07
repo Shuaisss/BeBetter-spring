be5c42fb2d835e15dda331ca0a3b521c67e0b31f
shuaisss <2394997182@qq.com>
2020-06-07 17:17:18 +0800

    搭好基本架构，完成基本userController代码

--- src/main/java/cn/edu/scujcc/User/UserController.java
+++ src/main/java/cn/edu/scujcc/User/UserController.java
@@ -1,5 +1,27 @@
 package cn.edu.scujcc.User;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController //注册为spring成员 包含主动转换json等功能
+@RequestMapping("/user")//总的网址前缀 
 public class UserController {
+	private static final Logger logger = LoggerFactory.getLogger(UserService.class);
+	
+	@Autowired 
+	private UserService service;
+	
+	@PostMapping("/register") //与user拼接
+	public User register(@RequestBody User u) {//@RequestBody将用户从body中传出来
+		User result = null;
+		logger.debug("用户注册:"+u);//在user中要先写同string否则打印无意义
+		result = service.register(u);//调用service的注册
+		return result;
+	}
 
 }
